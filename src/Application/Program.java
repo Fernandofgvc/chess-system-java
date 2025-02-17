af88162ef4c5dffaa24161531fdb01e71fbd62ba
Fernandofgvc <ffgvc1405@gmail.com>
2025-02-17 08:57:13 -0300

    First class: Position

---
+++ src/Application/Program.java
@@ -0,0 +1,11 @@
+package Application;
+
+import BoardGame.Position;
+
+public class Program {
+    public static void main(String[] args) throws Exception {
+        
+        Position pos = new Position(3, 5);
+        System.out.println(pos);
+    }
+}
